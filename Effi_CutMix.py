467c25553c24e1fc16740631eff6432a77d366dd
jijohn01 <jijohn01@naver.com>
2021-12-26 15:46:05 +0900

    실행용

--- Effi_CutMix.py
+++ Effi_CutMix.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from tqdm import tqdm
-
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
